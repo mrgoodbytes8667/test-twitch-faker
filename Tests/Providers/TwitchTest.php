8c33b35a71999415ef30f3de5d8b12668e37d4a9
MrGoodBytes <bytes@goodbytes.live>
2021-11-05 15:07:41 -0500

    Add login, displayName, broadcasterType, and Type methods

--- Tests/Providers/TwitchTest.php
+++ Tests/Providers/TwitchTest.php
@@ -4,6 +4,10 @@ namespace Bytes\Common\Faker\Tests\Providers;
 
 use Bytes\Common\Faker\Twitch\TestTwitchFakerTrait;
 use Generator;
+use PHPUnit\Framework\Constraint\IsEmpty;
+use PHPUnit\Framework\Constraint\IsEqual;
+use PHPUnit\Framework\Constraint\LogicalOr;
+use PHPUnit\Framework\Constraint\RegularExpression;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -36,6 +40,16 @@ class TwitchTest extends TestCase
     }
 
     /**
+     * @return Generator
+     */
+    public function provideSmallLoops()
+    {
+        foreach (range(1, 3) as $index) {
+            yield [$index];
+        }
+    }
+
+    /**
      * Test is mostly for coverage, there isn't a good way to test this function
      * @dataProvider provideLoops
      * @param int $index
@@ -62,7 +76,7 @@ class TwitchTest extends TestCase
      */
     public function testRateLimit()
     {
-        foreach(range(1, 10) as $j) {
+        foreach (range(1, 10) as $j) {
             foreach (range(-5, 65) as $index) {
                 $result = $this->faker->rateLimit($index);
                 $this->assertGreaterThanOrEqual(5, $result);
@@ -86,5 +100,116 @@ class TwitchTest extends TestCase
         $this->assertEquals(0, $result['RateLimit-Remaining']);
         $this->assertArrayHasKey('RateLimit-Reset', $result);
 
+    }
+
+    /**
+     * @dataProvider provideLoops
+     * @param int $index
+     */
+    public function testLogin($index)
+    {
+        $result = $this->faker->login();
+        $this->assertMatchesRegularExpression('/^[a-z0-9_]{4,25}$/', $result);
+    }
+
+    /**
+     * @dataProvider provideSmallLoops
+     * @param int $index
+     */
+    public function testLoginWithArgs($index)
+    {
+        foreach (range(3, 26) as $minChars) {
+            foreach (range(3, 26) as $maxChars) {
+                $result = $this->faker->login($minChars, $maxChars);
+                $this->assertMatchesRegularExpression('/^[a-z0-9_]{4,25}$/', $result);
+            }
+        }
+    }
+
+    /**
+     * @dataProvider provideLoops
+     * @param int $index
+     */
+    public function testDisplayName($index)
+    {
+        $result = $this->faker->displayName();
+        $this->assertMatchesRegularExpression('/^[a-zA-Z0-9_]{4,25}$/', $result);
+    }
+
+    /**
+     * @dataProvider provideSmallLoops
+     * @param int $index
+     */
+    public function testDisplayNameWithArgs($index)
+    {
+        foreach (range(3, 26) as $minChars) {
+            foreach (range(3, 26) as $maxChars) {
+                $result = $this->faker->displayName($minChars, $maxChars);
+                $this->assertMatchesRegularExpression('/^[a-zA-Z0-9_]{4,25}$/', $result);
+            }
+        }
+    }
+
+    /**
+     * @dataProvider provideLoops
+     * @param int $index
+     */
+    public function testBroadcasterType($index)
+    {
+        $result = $this->faker->broadcasterType();
+
+        $logicalOr = new LogicalOr();
+        $logicalOr->setConstraints([
+            new RegularExpression('/^staff|admin|global_mod$/'),
+            new IsEmpty(),
+        ]);
+
+        static::assertThat(
+            $result,
+            $logicalOr
+        );
+    }
+
+    /**
+     * @dataProvider provideLoops
+     * @param int $index
+     */
+    public function testType($index)
+    {
+        $result = $this->faker->type();
+
+        $logicalOr = new LogicalOr();
+        $logicalOr->setConstraints([
+            new RegularExpression('/^partner|affiliate$/'),
+            new IsEmpty(),
+        ]);
+
+        static::assertThat(
+            $result,
+            $logicalOr
+        );
+    }
+
+    /**
+     * @dataProvider provideLoops
+     * @param int $index
+     */
+    public function testStreamType($index)
+    {
+        $result = $this->faker->streamType();
+
+        $logicalOr = new LogicalOr();
+        $logicalOr->setConstraints([
+            new IsEqual('live'),
+            new IsEqual('playlist'),
+            new IsEqual('watch_party'),
+            new IsEqual('premiere'),
+            new IsEqual('rerun'),
+        ]);
+
+        static::assertThat(
+            $result,
+            $logicalOr
+        );
     }
 }
